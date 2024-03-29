2b9d1f02468ede204d97416b2c60b86b4de7bcc7
KareemGamal07 <kareemgmal7755@gmail.com>
2024-03-30 00:24:14 +0200

    Updates

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -10,41 +10,44 @@ import Footer from "../modules/Footer";
 import { RegType, RootState } from "../types";
 
 export default function page() {
-    const { id, isLoggedIn, cartEvent } = useSelector((state: RootState) => state.user)
-    const lang = useSelector((state: RootState) => state.translations.language)
-    const [data, setData] = useState<RegType | {}>({})
+  const { id, isLoggedIn, cartEvent } = useSelector(
+    (state: RootState) => state.user
+  );
+  const lang = useSelector((state: RootState) => state.translations.language);
+  const [data, setData] = useState<RegType | {}>({});
 
-    useEffect(() => {
-        const handelData = async () => {
-            try {
-                const url = `https://depot-data.onrender.com/users/${id}`;
-                const response = await fetch(url);
-                const data = await response.json();
-                setData(data.cart)
-            } catch {
-                console.log("error");
-            }
-        }
-        isLoggedIn && handelData()
-    }, [id, cartEvent])
+  useEffect(() => {
+    const handelData = async () => {
+      try {
+        const url = `https://depot-data.onrender.com/users/${id}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setData(data.cart);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    isLoggedIn && handelData();
+  }, [id, cartEvent]);
 
-    return (
-        <>
-            <TitleCartPage />
-            {
-                Object.keys(data).length > 0 || Object.keys(data).length > 0 ?
-                    <div
-                        style={{
-                            direction: lang == "ar" ? "rtl" : "ltr",
-                            fontFamily: lang == "ar" ? "Cairo, sans-serif" : "inherit"
-                        }}
-                        className="container mx-auto flex bigTablet:flex-row flex-col gap-8">
-                        <ShoppingCartSection />
-                        <CartTotalSection />
-                    </div> :
-                    <CartEmpty />
-            }
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <TitleCartPage />
+      {Object.keys(data).length > 0 || Object.keys(data).length > 0 ? (
+        <div
+          style={{
+            direction: lang == "ar" ? "rtl" : "ltr",
+            fontFamily: lang == "ar" ? "Cairo, sans-serif" : "inherit",
+          }}
+          className="container mx-auto flex bigTablet:flex-row flex-col gap-8"
+        >
+          <ShoppingCartSection />
+          <CartTotalSection />
+        </div>
+      ) : (
+        <CartEmpty />
+      )}
+      <Footer />
+    </>
+  );
 }
--- app/modules/productDetailsPage/AddtoWishList/AddtoWishList.tsx
+++ app/modules/productDetailsPage/AddtoWishList/AddtoWishList.tsx
@@ -9,16 +9,20 @@ import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../wishList/Loader";
 
 type propsType = {
-  product: ProductsTypes
-}
+  product: ProductsTypes;
+};
 
-export default function AddToWishList(props: propsType) {
-  const { name, isLoggedIn, id } = useSelector((state: RootState) => state.user)
-  const { addToWishlist } = useSelector((state: RootState) => state.translations.translations)
-  const [inList, setInList] = useState<boolean>(false)
-  const [loading, setLoading] = useState<boolean>(false)
-  const dispatch = useDispatch()
-  const [showNotification, setShowNotification] = useState(false)
+export default function AddToWishlist({ product }: propsType) {
+  const { name, isLoggedIn, id } = useSelector(
+    (state: RootState) => state.user
+  );
+  const { addToWishlist } = useSelector(
+    (state: RootState) => state.translations.translations
+  );
+  const [inList, setInList] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const dispatch = useDispatch();
+  const [showNotification, setShowNotification] = useState(false);
 
   useEffect(() => {
     const handelWishList = async () => {
@@ -26,75 +30,85 @@ export default function AddToWishList(props: propsType) {
         const url = `https://depot-data.onrender.com/users/${id}`;
         const response = await fetch(url);
         const data = await response.json();
-        dispatch(addToWishListNumber(data.wishList.length))
-        const foundElement = data.wishList.find((item: ProductsTypes) => item.id === props.product.id)
+        dispatch(addToWishListNumber(data.wishList.length));
+        const foundElement = data.wishList.find(
+          (item: ProductsTypes) => item.id === product.id
+        );
         if (foundElement) {
-          setInList(true)
+          setInList(true);
         }
       } catch (error) {
-        console.error('Error updating data:', error);
-        setInList(false)
+        console.error("Error updating data:", error);
+        setInList(false);
       }
-    }
-    name !== "" && handelWishList()
-    isLoggedIn == false && setInList(false)
-  }, [name])
+    };
+    name !== "" && handelWishList();
+    isLoggedIn == false && setInList(false);
+  }, [name]);
 
   const handelWishList = async () => {
     try {
       const url = `https://depot-data.onrender.com/users/${id}`;
       const response = await fetch(url);
       const data = await response.json();
-      const foundElement = data.wishList.find((item: ProductsTypes) => item.id === props.product.id)
+      const foundElement = data.wishList.find(
+        (item: ProductsTypes) => item.id === product.id
+      );
       if (foundElement) {
-        console.log('Array contains the element');
+        console.log("Array contains the element");
       } else {
-        data.wishList.push(props.product);
+        data.wishList.push(product);
         setInList(true);
         dispatch(addToWishListNumber(data.wishList.length));
         setLoading(false);
       }
       const updatedData = data;
       await fetch(url, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(updatedData)
+        body: JSON.stringify(updatedData),
       });
     } catch (error) {
-      console.error('Error updating data:', error);
+      console.error("Error updating data:", error);
     }
-  }
+  };
 
   const handelAddToWishList = () => {
     if (isLoggedIn == true) {
-      setLoading(true)
-      handelWishList()
+      setLoading(true);
+      handelWishList();
     } else {
-      setShowNotification(true)
+      setShowNotification(true);
     }
-  }
+  };
 
   setTimeout(() => {
-    setShowNotification(false)
-  }, 3000)
+    setShowNotification(false);
+  }, 3000);
 
   return (
     <>
-      {
-        loading ?
-          <Loader /> :
-          inList ?
-            <Link className="flex flex-row items-center gap-4 mt-4 mb-14" href="/wishList">
-              <IconHeartFilled style={{ display: "inline" }} size={18} />
-              {addToWishlist}
-            </Link> :
-            <button className="flex flex-row items-center gap-4 mt-4 mb-14" onClick={handelAddToWishList}>
-              <IconHeartFilled style={{ display: "inline" }} size={18} />
-              {addToWishlist}
-            </button>
-      }
+      {loading ? (
+        <Loader />
+      ) : inList ? (
+        <Link
+          className="flex flex-row items-center gap-4 mt-4 mb-14"
+          href="/wishList"
+        >
+          <IconHeartFilled style={{ display: "inline" }} size={18} />
+          {addToWishlist}
+        </Link>
+      ) : (
+        <button
+          className="flex flex-row items-center gap-4 mt-4 mb-14"
+          onClick={handelAddToWishList}
+        >
+          <IconHeartFilled style={{ display: "inline" }} size={18} />
+          {addToWishlist}
+        </button>
+      )}
     </>
   );
 }
